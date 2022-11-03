fbd49a6b281e304ccdbe16b3a41f806ea68f86f3
HuangHuangChia <72678149+HuangHuangChia@users.noreply.github.com>
2022-11-03 20:24:39 +0800

    Add files via upload

---
+++ LineNotify_Ngrok.bat
@@ -0,0 +1,10 @@
+set General=nU5A6LmAO0N5srqtUN4Tl4hjjED20Ow0LLgCxYzTbK2
+set Ngrok=CyBkMwsePVYW16lbOlRqtLEmg3O05HSBssTMgd2Vwjm
+
+set Token=%Ngrok%
+
+D:\Task\LineNotify\LineNotify_general.exe %Token% "D:\Task\LineNotifyNgrok\test.txt"
+REM nU5A6LmAO0N5srqtUN4Tl4hjjED20Ow0LLgCxYzTbK2    General(一般測試用)
+REM lj7BBFFqnWuoezANZEd5jD3NJu9dZkJFua3Cn4wIz2q 吳主任群組(資訊室通知)
+REM 0xQH1k7AuD1CERF9Hy12XNv8MnTOE8V6MwitbfdJHe9   桃療資訊室小組
+rem CyBkMwsePVYW16lbOlRqtLEmg3O05HSBssTMgd2Vwjm   Ngrok
